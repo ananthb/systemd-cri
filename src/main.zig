806173e278062d8c2aadb7acbffc3665b29b9221
Ananth Bhaskararaman <ananth.b@qubecinema.com>
2024-12-23 03:22:54 +0530

    feat: Try to open a dbus connection

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,16 @@
 const std = @import("std");
+const libsystemd = @cImport({
+    @cInclude("systemd/sd-bus.h");
+});
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var bus = libsystemd.bus{};
+    const bus_open_result = libsystemd.sd_bus_open_with_description(&bus, "systemd-cri", null);
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    if (bus_open_result < 0) {
+        std.debug.print("failed to open systemd bus: {}\n", .{bus_open_result});
+        return;
+    }
 }
 
 test "simple test" {
